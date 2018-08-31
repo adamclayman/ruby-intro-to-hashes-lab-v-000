d91eba5a079d3078f7ea4b439e77db86ae987304
Adam Clayman <adam@clayman.org>
2018-08-31 18:49:25 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,9 @@ end
 def monopoly_with_second_tier
 	monopoly = {
 		railroads: {
-			names: {}
+			pieces: 4,
+			names: {},
+			rent_in_dollars: {}
 		}
 	}
 end
@@ -25,7 +27,14 @@ end
 def monopoly_with_third_tier
 	monopoly = {
 		railroads: {
-			names: {}
+			pieces: 4,
+			names: {},
+			rent_in_dollars: {
+				one_piece_owned: 25,
+				two_pieces_owned: 50,
+				three_pieces_owned: 100,
+				four_pieces_owned: 200
+			}
 		}
 	}
 end
